6b5d7bc94dcc83451e5518f82ba7f8e60d39a23f
Via <zhangyawei@stu.xjtu.edu.cn>
2018-06-07 17:50:10 +0800

    commit practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,40 @@
 package practice09;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Klass {
+    private int number;
+    private Person leader;
+    private Set<Person> members;
+
+    public Klass(){}
+
+    public Klass(int num){
+        number = num;
+        members = new HashSet<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public Person getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Person leader) {
+        if (members.contains(leader))
+            this.leader = leader;
+        else
+            System.out.print("It is not one of us.\n");
+    }
+
+    public void appendMember(Student student){
+        members.add(student);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,33 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass kclass;
+
+    public Teacher(int item, String name, int age){
+        super(item, name, age);
+    }
+    public Teacher(int item, String name, int age, Klass kclass){
+        super(item, name,age);
+        this.kclass = kclass;
+    }
+
+    public Klass getKlass() {
+        return kclass;
+    }
+
+    @Override
+    public String introduce() {
+        String intro = super.introduce();
+        if (null == kclass)
+            return intro + " I am a Teacher. I teach No Class.";
+        else
+            return intro + " I am a Teacher. I teach Class " + kclass.getNumber() + ".";
+    }
+
+    public String introduceWith(Student student){
+        if (student.getKlass().getNumber() == this.kclass.getNumber())
+            return "My name is " + this.getName() +". I am " + this.getAge() + " years old. I am a Teacher. I teach " + student.getName() + ".";
+        else
+            return "My name is " + this.getName() +". I am " + this.getAge() + " years old. I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }
