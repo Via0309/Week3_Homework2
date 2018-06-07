c0db3664162f967d375df22b82ecb346bd906d1e
Via <zhangyawei@stu.xjtu.edu.cn>
2018-06-07 18:15:11 +0800

    commit practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,47 @@
 package practice10;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Klass {
+    private int number;
+    private Person leader;
+    private Set<Integer> members;
+
+    public Klass(){}
+
+    public Klass(int num){
+        number = num;
+        members = new TreeSet<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public Person getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Person leader) {
+        if (members.contains(leader.getId()))
+            this.leader = leader;
+        else
+            System.out.print("It is not one of us.\n");
+    }
+
+    public void appendMember(Student student){
+        members.add(student.getId());
+    }
+
+    public boolean isIn(Student student){
+        if (student.getKlass().getNumber() == number)
+            return true;
+        return false;
+    }
 }
--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,38 @@
 package practice10;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(){
+        name = "";
+        age = 0;
+    }
+
+    public Person(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    public boolean equals(Person person){
+        return (age == person.getAge()) && (name.equals(person.getName()));
+    }
 }
