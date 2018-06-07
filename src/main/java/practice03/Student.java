46f740029c3eb5997ea8d3a12cbc1099f5e927f7
Via <zhangyawei@stu.xjtu.edu.cn>
2018-06-07 16:57:13 +0800

    commit practice03

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,21 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    private int kclass;
+
+    public Student(){}
+
+    public Student(String name, int age, int kclass){
+        super(name,age);
+        this.kclass = kclass;
+    }
+
+    public int getKlass() {
+        return kclass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class " + kclass + ".";
+    }
 }
