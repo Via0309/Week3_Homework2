574955f87114a2a503738cac6a338d15c5d6463c
Via <zhangyawei@stu.xjtu.edu.cn>
2018-06-07 17:01:05 +0800

    commit practice04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,15 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(){}
+
+    public Worker(String name, int age){
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        String intro = super.introduce();
+        return intro + " I am a Worker. I have a job.";
+    }
 }
