be82e1f3fdde3a4fbcfa9fd3acebc3bd5c71166c
Via <zhangyawei@stu.xjtu.edu.cn>
2018-06-07 17:11:52 +0800

    commit practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+    private int kclass;
+
+    public Student(){}
+
+    public Student(String name, int age, int kclass){
+        super(name,age);
+        this.kclass = kclass;
+    }
+
+    public int getKlass() {
+        return kclass;
+    }
+
+    @Override
+    public String introduce() {
+        String intro = super.introduce();
+        return intro + " I am a Student. I am at Class " + kclass + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int kclass;
+
+    public Teacher(){}
+
+    public Teacher(String name, int age){
+        super(name, age);
+        this.kclass = 0;
+    }
+    public Teacher(String name, int age, int kclass){
+        super(name,age);
+        this.kclass = kclass;
+    }
+
+    public int getKlass() {
+        return kclass;
+    }
+
+    @Override
+    public String introduce() {
+        String intro = super.introduce();
+        if (0 == kclass)
+            return intro + " I am a Teacher. I teach No Class.";
+        else
+            return intro + " I am a Teacher. I teach Class " + kclass + ".";
+    }
 }
